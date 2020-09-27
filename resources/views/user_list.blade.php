04995ec7dd893ee622808dfc79cebd6239285f2b
kota1130 <40798596+kota1130@users.noreply.github.com>
2020-09-28 00:44:46 +0900

    Update user_list.blade.php

--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -1,1 +1,9 @@
-<div class = "container">入門課題2です</div>
+入門課題です<br>
+
+@foreach($values as $value)
+
+id:{{$value->id}}<br>
+user_id:{{$value->user_id}}<br>
+password:{{$value->password}}<br>
+=========================<br>
+@endforeach
