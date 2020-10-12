db4e3147690721f03ffdd17b951a4cbb8c657317
kota1130 <40798596+kota1130@users.noreply.github.com>
2020-10-13 00:36:22 +0900

    Update UsersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 
 class UsersController extends Controller
 {
-    public function index(){
-        return view('user_list');
-    }
+    public function search(Request $request){
+
+        $users = \DB::select('select * from users');
+        return view('user_list',['users' => $users]);
+    
 }  
+}
