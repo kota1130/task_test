d4348a706d77ccbc88085e967409a2f6e33d27e4
kota1130 <40798596+kota1130@users.noreply.github.com>
2020-10-26 01:09:35 +0900

    Update UsersController.php
    データ取得方法を修正しました。

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 
 class UsersController extends Controller
 {
     public function search(Request $request){
 
-        $users = \DB::select('select * from users');
+        $users = User::all();
         return view('user_list',['users' => $users]);
     
-}  
+    }  
 }
