ddc10e826ff89d88f1f2c5e845c554da8cf668b3
kota1130 <40798596+kota1130@users.noreply.github.com>
2020-10-26 01:10:53 +0900

    Update user_list.blade.php
    HTML記載方法を修正し、テーブル表記にいたしました。

--- resources/views/user_list.blade.php
+++ resources/views/user_list.blade.php
@@ -1,9 +1,26 @@
-@foreach($users as $user)
-        
-       id: {{$user->id}}<br>
-       user_id: {{$user->user_id}}<br>
-       password: {{$user->password}}<br>
-       =========================<br>
-   
-        
-    @endforeach
+<!DOCTYPE html>
+<html lang="ja">
+　<head>
+　　<meta charset="utf-8">
+　　<title>テスト</title>
+　</head>
+　<body>
+   <table border="1">
+    <tr>
+     <th>id</th>
+     <th>user_id</th>
+     <th>password</th>
+    </tr>
+    @foreach($users as $user)
+    <tr>
+     <td>{{$user->id}}</td>
+     <td>{{$user->user_id}}</td>
+     <td>{{$user->password}}</td>
+    </tr>
+    @endforeach 
+   </table>
+　</body>
+</html>
+
+
+
